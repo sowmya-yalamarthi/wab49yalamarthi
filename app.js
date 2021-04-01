c1d4da76fdc98ba730b28921e028c5e9b83b1519
sowmya-yalamarthi <s542049@nwmissouri.edu>
2021-03-31 20:31:18 -0500

    changes madeg

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogRouter = require('./routes/dog');
+var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 
 
 var app = express();
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dog', dogRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
